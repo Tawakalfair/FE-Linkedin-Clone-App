77e000e3a3b27fac5744a3a41bae3462504bf15b
Tawakalfair <fairtawakal@gmail.com>
2022-07-18 17:38:34 +0700

    initial

---
+++ src/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Header.css";
+import HeaderOption from "./HeaderOption";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"
+          alt=""
+        />
+
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notification" />
+        <HeaderOption
+          avatar="https://cdn.kincir.com/1/production/media/2017/desember/ian-mckellen-ingin-kembali-memerankan-gandalf-di-serial-lord-of-the-rings/gandalf-700x700.jpg"
+          title="me"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
